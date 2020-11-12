3dc282ee5ea5067f39720e6e9759199cc23f9f7d
KazDragon <matthew.chaplain@gmail.com>
2020-11-12 10:04:32 +0100

    Implemented Codacy suggestions
    * Quoted various path environment variables to prevent globbing
      and word splitting.
    * Also removed erroneously-added CMakeListsConan.txt file.

--- .travis/fetch_system_dependencies.sh
+++ .travis/fetch_system_dependencies.sh
@@ -3,14 +3,14 @@ if [ -z ${EXTERNAL_ROOT+x} ]; then echo EXTERNAL_ROOT not set; exit 1; fi
 
 export EXTERNAL_BUILD_ROOT=$HOME/external_build
 
-mkdir $EXTERNAL_BUILD_ROOT || true
+mkdir "$EXTERNAL_BUILD_ROOT" || true
 
 # Install gsl-lite dependency
 if [ ! -f "$EXTERNAL_ROOT/gsl-lite/include/gsl/gsl-lite.hpp" ]; then
-    cd $EXTERNAL_BUILD_ROOT;
+    cd "$EXTERNAL_BUILD_ROOT";
     wget https://github.com/gsl-lite/gsl-lite/archive/v0.34.0.tar.gz;
     tar -xzf v0.34.0.tar.gz;
     cd gsl-lite-0.34.0;
-    cmake -DCMAKE_INSTALL_PREFIX=$EXTERNAL_ROOT/gsl-lite -DGSL_LITE_OPT_BUILD_TESTS=Off .;
+    cmake -DCMAKE_INSTALL_PREFIX="$EXTERNAL_ROOT/gsl-lite" -DGSL_LITE_OPT_BUILD_TESTS=Off .;
     make -j2 && make install;
 fi
