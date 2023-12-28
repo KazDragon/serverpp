ee9c0c3b7f3b40854c88858dc5f294d399cd0863
Matthew Chaplain <matthew.chaplain@gmail.com>
2023-12-28 09:23:15 +0100

    Added automatic formatting, vscode build presets

--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,17 +1,14 @@
-#include <serverpp/tcp_server.hpp>
 #include <boost/asio/io_context.hpp>
 #include <cstdlib>
+#include <serverpp/tcp_server.hpp>
 
 int main()
 {
-    try
-    {
-        boost::asio::io_context ctx;
-        serverpp::tcp_server server{ctx, 0, [](auto &&...){}};
-    }
-    catch(...)
-    {
-    }
+  try {
+    boost::asio::io_context ctx;
+    serverpp::tcp_server server{ctx, 0, [](auto &&...) {}};
+  } catch (...) {
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
